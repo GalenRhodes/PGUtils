9fabfc51038f860f4548b90bc1d4335347e3db25
GalenRhodes <galenrhodes@me.com>
2023-06-22 12:18:17 -0400

    No comment.

---
+++ src/main/java/com/projectgalen/lib/utils/ProgressReporter.java
@@ -0,0 +1,46 @@
+package com.projectgalen.lib.utils;
+
+// ===========================================================================
+//     PROJECT: PGUtils
+//    FILENAME: ProgressReporter.java
+//         IDE: IntelliJ IDEA
+//      AUTHOR: Galen Rhodes
+//        DATE: June 22, 2023
+//
+// Copyright © 2023 Project Galen. All rights reserved.
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+// IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// ===========================================================================
+
+@SuppressWarnings("unused")
+public interface ProgressReporter {
+    String getMessage();
+
+    int getProgress();
+
+    int getProgressMax();
+
+    int getProgressMin();
+
+    String getProgressText();
+
+    void setMessage(String text);
+
+    void setProgress(int value);
+
+    void setProgressMax(int value);
+
+    void setProgressMin(int value);
+
+    void setProgressText(String text);
+}
