238da02793d577f2f1c977ce9ed24007f49f553a
GalenRhodes <galenrhodes@me.com>
2023-08-25 17:25:19 -0400

    No Comment.

---
+++ src/main/java/com/projectgalen/lib/utils/collections/ring/AbstractRingBuffer.java
@@ -0,0 +1,223 @@
+package com.projectgalen.lib.utils.collections.ring;
+
+// ===========================================================================
+//     PROJECT: PGUtils
+//    FILENAME: AbstractRingBuffer.java
+//         IDE: IntelliJ IDEA
+//      AUTHOR: Galen Rhodes
+//        DATE: August 25, 2023
+//
+// Copyright © 2023 Project Galen. All rights reserved.
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+// IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// ===========================================================================
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+import java.lang.reflect.Array;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+@SuppressWarnings({ "SuspiciousSystemArraycopy", "unchecked" })
+public abstract class AbstractRingBuffer<E> implements AutoCloseable {
+
+    protected final int     initSize;
+    protected final String  lock   = UUID.randomUUID().toString();
+    protected       int     head   = 0;
+    protected       int     tail   = 0;
+    protected       boolean closed = false;
+    protected       int     capacity;
+    protected       Object  buffer;
+
+    public AbstractRingBuffer(int initSize) {
+        this.initSize = Math.max(initSize, 1024 * 1024);
+        this.capacity = this.initSize;
+        this.buffer   = createArray(this.initSize);
+    }
+
+    public AbstractRingBuffer() {
+        this(1024 * 1024);
+    }
+
+    public int available() {
+        return getLocked(false, this::count);
+    }
+
+    public int capacity() {
+        return getLocked(false, () -> capacity);
+    }
+
+    public void close() {
+        doLocked(() -> {
+            closed = true;
+            head   = tail = 0;
+            buffer = null;
+        });
+    }
+
+    public boolean isClosed() {
+        return getLocked(false, () -> closed);
+    }
+
+    public boolean isEmpty() {
+        return getLocked(false, () -> (head == tail));
+    }
+
+    public int skip(long n) {
+        return getLocked(() -> {
+            int len = (int)Math.max(Math.min(n, (closed ? 0 : count())), 0);
+            incHead(len);
+            return len;
+        });
+    }
+
+    public <T> Optional<T> waitFor(@NotNull Supplier<Boolean> stopOn, @NotNull Supplier<T> supplier) throws InterruptedException {
+        synchronized(lock) { return wait1(stopOn) ? Optional.empty() : Optional.ofNullable(supplier.get()); }
+    }
+
+    public <T> Optional<T> waitFor(@NotNull Supplier<T> supplier) throws InterruptedException {
+        return waitFor(() -> false, supplier);
+    }
+
+    protected abstract @NotNull Object createArray(@Range(from = 0, to = Integer.MAX_VALUE) int size);
+
+    protected void doLocked(@NotNull Runnable runnable) {
+        synchronized(lock) { try { runnable.run(); } finally { lock.notify(); } }
+    }
+
+    protected @NotNull Optional<Optional<E>> get1() {
+        return getLocked(() -> Optional.ofNullable(closed ? null : get2()));
+    }
+
+    protected int get1(@NotNull Object buf, int off, int len) {
+        return getLocked(() -> get2(buf, off, len));
+    }
+
+    protected @NotNull Optional<E> get2() {
+        return ((head == tail) ? Optional.empty() : get3());
+    }
+
+    protected int get2(@NotNull Object buf, int off, int len) {
+        return (closed ? -1 : cpOut(buf, off, len));
+    }
+
+    protected <T> T getLocked(@NotNull Supplier<T> supplier) {
+        return getLocked(true, supplier);
+    }
+
+    protected <T> T getLocked(boolean notify, @NotNull Supplier<T> supplier) {
+        synchronized(lock) { try { return supplier.get(); } finally { if(notify) lock.notify(); } }
+    }
+
+    protected void put1(@NotNull Object buf, int off, int len) {
+        if(getLocked(() -> put2(buf, off, len))) throw new IllegalStateException("Ring buffer is closed.");
+    }
+
+    protected void put1(@NotNull E value) {
+        if(getLocked(() -> put2(value))) throw new IllegalStateException("Ring buffer is closed.");
+    }
+
+    protected boolean put2(@NotNull Object buf, int off, int len) {
+        if(closed) return true;
+        ensure(len);
+        cpIn(buf, off, len);
+        return false;
+    }
+
+    protected boolean put2(@NotNull E value) {
+        if(closed) return true;
+        ensure(1);
+        Array.set(buffer, incTail(1), value);
+        return false;
+    }
+
+    private int count() {
+        return ((head <= tail) ? (tail - head) : ((capacity - head) + tail));
+    }
+
+    private void cpIn(@NotNull Object buf, int off, int len) {
+        if(tail <= head) {
+            if(tail == head) head = tail = 0;
+            System.arraycopy(buf, off, buffer, incTail(len), len);
+        }
+        else {
+            int l1 = Math.min(len, (capacity - tail));
+            System.arraycopy(buf, off, buffer, incTail(len), l1);
+            System.arraycopy(buf, (off + l1), buffer, 0, (len - l1));
+        }
+    }
+
+    private int cpOut(@NotNull Object buf, int off, int len) {
+        if((head == tail) || (len <= 0)) return 0;
+        int l1 = Math.min(len, count());
+        if(head < tail) {
+            System.arraycopy(buffer, incHead(l1), buf, off, l1);
+        }
+        else {
+            int l2 = Math.min(l1, (capacity - head));
+            System.arraycopy(buffer, incHead(l1), buf, off, l2);
+            System.arraycopy(buffer, 0, buf, (off + l2), (l1 - l2));
+        }
+        return l1;
+    }
+
+    private void ensure(int needed) {
+        if(needed > 0) {
+            int cap = capacity;
+            int cnt = count();
+
+            if((cap - cnt - 1) < needed) {
+                do cap = incSize(cap); while((cap - cnt - 1) < needed);
+                Object buf = createArray(cap);
+                cpOut(buf, 0, cnt);
+                head     = 0;
+                tail     = cnt;
+                buffer   = buf;
+                capacity = cap;
+            }
+        }
+    }
+
+    private @NotNull Optional<E> get3() {
+        return Optional.of((E)Array.get(buffer, incHead(1)));
+    }
+
+    private int incHead(@Range(from = 0, to = Integer.MAX_VALUE) int delta) {
+        int orgHead = head;
+        head = ((head + delta) % capacity);
+        return orgHead;
+    }
+
+    private int incSize(int sz) {
+        long l = (long)Math.floor(sz * 1.75);
+        if(l <= Integer.MAX_VALUE) return (int)l;
+        throw new IllegalStateException("Resulting Buffer Too Large.");
+    }
+
+    private int incTail(@Range(from = 0, to = Integer.MAX_VALUE) int delta) {
+        int orgTail = tail;
+        tail = ((tail + delta) % capacity);
+        return orgTail;
+    }
+
+    private boolean wait1(@NotNull Supplier<Boolean> stopOn) throws InterruptedException {
+        do {
+            if(head != tail) return false;
+            if(closed || stopOn.get()) return true;
+            lock.wait();
+        }
+        while(true);
+    }
+}
---
+++ src/main/java/com/projectgalen/lib/utils/collections/ring/ByteRingBuffer.java
@@ -0,0 +1,104 @@
+package com.projectgalen.lib.utils.collections.ring;
+
+// ===========================================================================
+//     PROJECT: PGUtils
+//    FILENAME: ByteRingBuffer.java
+//         IDE: IntelliJ IDEA
+//      AUTHOR: Galen Rhodes
+//        DATE: August 25, 2023
+//
+// Copyright © 2023 Project Galen. All rights reserved.
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+// SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+// IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// ===========================================================================
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InterruptedIOException;
+import java.io.OutputStream;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+public class ByteRingBuffer extends AbstractRingBuffer<Byte> {
+
+    public ByteRingBuffer(int initSize)                                                                { super(initSize); }
+
+    public ByteRingBuffer()                                                                            { super(); }
+
+    public int get()                                                                                   { return get1().map(this::bar).orElse(-2); }
+
+    public int get(byte @NotNull [] buf, int off, int len)                                             { return get1(buf, off, len); }
+
+    public int get(byte @NotNull [] buf)                                                               { return get1(buf, 0, buf.length); }
+
+    public void put(int aByte)                                                                         { put1((byte)(aByte & 0x00ff)); }
+
+    public void put(byte @NotNull [] buf, int off, int len)                                            { put1(buf, off, len); }
+
+    public void put(byte @NotNull [] buf)                                                              { put1(buf, 0, buf.length); }
+
+    public int wget(byte @NotNull [] buf, int off, int len) throws InterruptedException                { return waitFor(() -> get2(buf, off, len)).orElse(-1); }
+
+    public int wget(byte @NotNull [] buf) throws InterruptedException                                  { return waitFor(() -> get2(buf, 0, buf.length)).orElse(-1); }
+
+    public int wget() throws InterruptedException                                                      { return waitFor(() -> bar(get2())).orElse(-1); }
+
+    protected @Override @NotNull Object createArray(@Range(from = 0, to = Integer.MAX_VALUE) int size) { return new byte[size]; }
+
+    private int bar(@NotNull Optional<Byte> ob)                                                        { return ob.map(this::map).orElse(-1); }
+
+    private int map(byte b)                                                                            { return (b & 0x00ff); }
+
+    private final class RingInputStream extends InputStream {
+        private boolean strmClsd = false;
+
+        public RingInputStream()                                                           { super(); }
+
+        public int available()                                                             { return ByteRingBuffer.this.available(); }
+
+        public @Override void close()                                                      { doLocked(() -> strmClsd = true); }
+
+        public @Override int read() throws IOException                                     { return foo(() -> bar(get2())).orElse(-1); }
+
+        public @Override int read(byte @NotNull [] b, int off, int len) throws IOException { return foo(() -> get2(b, off, len)).orElse(-1); }
+
+        public @Override long skip(long n)                                                 { return ByteRingBuffer.this.skip(n); }
+
+        private <T> Optional<T> foo(@NotNull Supplier<T> s) throws IOException             { try { return waitFor(() -> strmClsd, s); } catch(Exception e) { throw new InterruptedIOException(); } }
+    }
+
+    private final class RingOutputStream extends OutputStream {
+
+        private boolean strmClsd = false;
+
+        public RingOutputStream()                                                            { super(); }
+
+        @Override public void close()                                                        { doLocked(() -> strmClsd = true); }
+
+        @Override public void write(byte @NotNull [] b, int off, int len) throws IOException { foo(() -> put2(b, off, len)); }
+
+        public @Override void write(int b) throws IOException                                { foo(() -> put2((byte)(b & 0x00ff))); }
+
+        private void foo(@NotNull Runnable runnable) throws IOException {
+            if(getLocked(() -> {
+                if(closed || strmClsd) return true;
+                runnable.run();
+                return false;
+            })) throw new IOException("Output stream is closed.");
+        }
+    }
+}
